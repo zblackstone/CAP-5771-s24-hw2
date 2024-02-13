96cb1b956518d102bf0dd761c28b1b2869084bc6
zblackstone <144168057+zblackstone@users.noreply.github.com>
2024-02-13 15:51:18 -0500

    Update all_questions.py

--- all_questions.py
+++ all_questions.py
@@ -23,40 +23,40 @@ def question1():
     level2_left = {}
     level2_right = {}
 
-    level1["smoking"] = 0.
-    level1["smoking_info_gain"] = 0.
+    level1["smoking"] = 1
+    level1["smoking_info_gain"] = 0.278
 
-    level1["cough"] = 0.
-    level1["cough_info_gain"] = 0.
+    level1["cough"] = 0
+    level1["cough_info_gain"] = 0.035
 
-    level1["radon"] = 0.
-    level1["radon_info_gain"] = 0.
+    level1["radon"] = 0
+    level1["radon_info_gain"] = 0.236
 
-    level1["weight_loss"] = 0.0
-    level1["weight_loss_info_gain"] = 0.
+    level1["weight_loss"] = 0
+    level1["weight_loss_info_gain"] = 0.029
 
-    level2_left["smoking"] = 0.
-    level2_left["smoking_info_gain"] = 0.
-    level2_right["smoking"] = 0.
-    level2_right["smoking_info_gain"] = 0.
+    level2_left["smoking"] = -1
+    level2_left["smoking_info_gain"] = -1
+    level2_right["smoking"] = -1
+    level2_right["smoking_info_gain"] = -1
 
-    level2_left["radon"] = 0.
-    level2_left["radon_info_gain"] = 0.
+    level2_left["radon"] = 1
+    level2_left["radon_info_gain"] = 0.722
 
-    level2_left["cough"] = 0.
-    level2_left["cough_info_gain"] = 0.
+    level2_left["cough"] = -1
+    level2_left["cough_info_gain"] = -1
 
-    level2_left["weight_loss"] = 0.
-    level2_left["weight_loss_info_gain"] = 0.
+    level2_left["weight_loss"] = -1
+    level2_left["weight_loss_info_gain"] = -1
 
-    level2_right["radon"] = 0.
-    level2_right["radon_info_gain"] = 0.
+    level2_right["radon"] = -1
+    level2_right["radon_info_gain"] = -1
 
-    level2_right["cough"] = 0.
-    level2_right["cough_info_gain"] = 0.
+    level2_right["cough"] = 1
+    level2_right["cough_info_gain"] = 0.722
 
-    level2_right["weight_loss"] = 0.
-    level2_right["weight_loss_info_gain"] = 0.
+    level2_right["weight_loss"] = -1
+    level2_right["weight_loss_info_gain"] = -1
 
     answer["level1"] = level1
     answer["level2_left"] = level2_left
@@ -79,18 +79,29 @@ def question2():
     answer = {}
 
     # Answers are floats
-    answer["(a) entropy_entire_data"] = 0.
+    answer["(a) entropy_entire_data"] = 1.425
     # Infogain
-    answer["(b) x <= 0.2"] = 0.
-    answer["(b) x <= 0.7"] = 0.
-    answer["(b) y <= 0.6"] = 0.
+    answer["(b) x <= 0.2"] = 1.190
+    answer["(b) x <= 0.7"] = 1.118
+    answer["(b) y <= 0.6"] = 0.945
 
     # choose one of 'x=0.2', 'x=0.7', or 'x=0.6'
-    answer["(c) attribute"] = ""  
+    answer["(c) attribute"] = "y=0.6"  
 
     # Use the Binary Tree structure to construct the tree
     # Answer is an instance of BinaryTree
-    tree = u.BinaryTree("Root")
+    tree = u.BinaryTree("y=0.6")
+    tree.left = u.BinaryTree("x=0.2")
+    tree.right = u.BinaryTree("x=0.7")
+    tree.left.left = u.BinaryTree("y=0.8")
+    tree.left.right = u.BinaryTree("Class A")
+    tree.left.left.left = u.BinaryTree("Class B")
+    tree.left.left.right = u.BinaryTree("Class C")
+    tree.right.right = u.BinaryTree("y=0.3")
+    tree.right.right.left = u.BinaryTree("Class C")
+    tree.right.right.right = u.BinaryTree("Class A")
+    tree.right.left = u.BinaryTree("Class B")
+
     answer["(d) full decision tree"] = tree
 
     return answer
@@ -103,18 +114,18 @@ def question3():
     answer = {}
 
     # float
-    answer["(a) Gini, overall"] = 0.
+    answer["(a) Gini, overall"] = 0.5
 
     # float
     answer["(b) Gini, ID"] = 0.0
-    answer["(c) Gini, Gender"] = 0.
-    answer["(d) Gini, Car type"] = 0.
-    answer["(e) Gini, Shirt type"] = 0.
+    answer["(c) Gini, Gender"] = 0.42
+    answer["(d) Gini, Car type"] = 0.156
+    answer["(e) Gini, Shirt type"] = 0.491
 
-    answer["(f) attr for splitting"] = ""
+    answer["(f) attr for splitting"] = "Car type"
 
     # Explanatory text string
-    answer["(f) explain choice"] = ""
+    answer["(f) explain choice"] = "Lowest GINI Index, i.e. lowest impurity compared to ID, Gender, and Shirt Type"
 
     return answer
 
@@ -135,46 +146,46 @@ def question4():
     #  'quantitative', 'interval', 'ratio'
     # If you have a choice between 'binary' and 'discrete', choose 'binary'
 
-    answer["a"] = []
+    answer["a"] = ['binary', 'qualitative', 'nominal']
 
     # Explain if there is more than one interpretation. Repeat for the other questions. At least five words that form a sentence.
-    answer["a: explain"] = ""
+    answer["a: explain"] = "Only two categories with no order or measure"
 
-    answer["b"] = []
-    answer["b: explain"] = ""
+    answer["b"] = ['continuous', 'quantitative', 'ratio']
+    answer["b: explain"] = "Range of values for brightness, true zero point"
 
-    answer["c"] = []
-    answer["c: explain"] = ""
+    answer["c"] = ['discrete', 'qualitative', 'ordinal']
+    answer["c: explain"] = "Can be categorized by judgement, but unable to precisely measure with the naked eye"
 
-    answer["d"] = []
-    answer["d: explain"] = ""
+    answer["d"] = ['continuous', 'quantitative', 'interval']
+    answer["d: explain"] = "Range of values with no true zero (0 is still an angle measurement)"
 
-    answer["e"] = []
-    answer["e: explain"] = ""
+    answer["e"] = ['discrete', 'qualitative', 'ordinal']
+    answer["e: explain"] = "Implied ordered categories in terms of final standings, but no quantifiable difference represented in the medals themselves"
 
-    answer["f"] = []
-    answer["f: explain"] = ""
+    answer["f"] = ['continuous', 'quantitative', 'ratio']
+    answer["f: explain"] = "Can be measured precisely and with a true zero"
 
-    answer["g"] = []
-    answer["g: explain"] = ""
+    answer["g"] = ['discrete', 'quantitative', 'ratio']
+    answer["g: explain"] = "Countable quantity with a true zero"
 
-    answer["h"] = []
-    answer["h: explain"] = ""
+    answer["h"] = ['discrete', 'qualitative', 'nominal']
+    answer["h: explain"] = "Categorization of books with no quantitative value or order"
 
-    answer["i"] = []
-    answer["i: explain"] = ""
+    answer["i"] = ['discrete', 'qualitative', 'ordinal']
+    answer["i: explain"] = "Categories with natural order with no measureable quantities"
 
-    answer["j"] = []
-    answer["j: explain"] = ""
+    answer["j"] = ['discrete', 'qualitative', 'ordinal']
+    answer["j: explain"] = "Categories with natural order with no measureable quantities"
 
-    answer["k"] = []
-    answer["k: explain"] = ""
+    answer["k"] = ['continuous', 'quantitative', 'ratio']
+    answer["k: explain"] = "Can be measured precisely and has a true zero point"
 
-    answer["l"] = []
-    answer["l: explain"] = ""
+    answer["l"] = ['continuous', 'quantitative', 'ratio']
+    answer["l: explain"] = "Can be measured precisely and has a true zero point"
 
-    answer["m"] = []
-    answer["m: explain"] = ""
+    answer["m"] = ['discrete', 'qualitative', 'nominal']
+    answer["m: explain"] = "No measurable quantities or order"
 
     return answer
 
@@ -188,18 +199,18 @@ def question5():
     # Read appropriate section of book chapter 3
 
     # string: one of 'Model 1' or 'Model 2'
-    explain["a"] = ""
-    explain["a explain"] = ""
+    explain["a"] = "Model 2"
+    explain["a explain"] = "Because Model 2 is pruned, the complexity of the decision tree is reduced. This will lead to better performance with any new data set as it has reduced the number of redundant sections and the possibility of overfitting"
 
     # string: one of 'Model 1' or 'Model 2'
-    explain["b"] = ""
-    explain["b explain"] = ""
+    explain["b"] = "Model 1"
+    explain["b explain"] = "Higher classification accuracy as outlined by the equations (.85 > .81"
 
-    explain["c similarity"] = ""
-    explain["c similarity explain"] = ""
+    explain["c similarity"] = "Both aim to prevent overfitting"
+    explain["c similarity explain"] = "Both MDL and PEE consider model complexity and discourage overly complex models to prevent overfitting"
 
-    explain["c difference"] = ""
-    explain["c difference explain"] = ""
+    explain["c difference"] = "How they work"
+    explain["c difference explain"] = "MDL uses description length to quantify the relationship between model complexity and the fit of the data. PEE adjusts the error rate by adding increases with complexity and decreases with confidence in the error estimate"
 
     return explain
 
@@ -213,21 +224,24 @@ def question6():
     # value of the form "z <= float" where "z" is "x" or "y"
     #  and "float" is a floating point number (notice: <=)
     # The value could also be "A" or "B" if it is a leaf
-    answer["a, level 1"] = ""
-    answer["a, level 2, right"] =""
-    answer["a, level 2, left"] = ""
-    answer["a, level 3, left"] = ""
-    answer["a, level 3, right"] = ""
+    answer["a, level 1"] = "x<=0.5"
+    answer["a, level 2, right"] = "A"
+    answer["a, level 2, left"] = "y<=0.4"
+    answer["a, level 3, left"] = "B"
+    answer["a, level 3, right"] = "A"
 
     # run each datum through the tree. Count the number of errors and divide by number of samples. .
     # Since we have areas: calculate the area that is misclassified (total area is unity)
     # float between 0 and 1
-    answer["b, expected error"] = 0.
+    answer["b, expected error"] = 0.06
 
     # Use u.BinaryTree to define the tree. Create your tree.
     # Replace "root node" by the proper node of the form "z <= float"
-    tree = u.BinaryTree("root note")
-
+    tree = u.BinaryTree("x<=0.5")
+    tree.left = u.Binarytree("y<=0.4")
+    tree.right = u.BinaryTree("A")
+    tree.left.left = u.BinaryTree("B")
+    tree.left.right = u.BinaryTree("A")
     answer["c, tree"] = tree
 
     return answer
@@ -238,19 +252,19 @@ def question7():
     answer = {}
 
     # float
-    answer["a, info gain, ID"] = 0.
-    answer["b, info gain, Handedness"] = 0.
+    answer["a, info gain, ID"] = 1
+    answer["b, info gain, Handedness"] = 0.531
 
     # string: "ID" or "Handedness"
-    answer["c, which attrib"] = ""
+    answer["c, which attrib"] = "ID"
 
     # answer is a float
-    answer["d, gain ratio, ID"] = 0.
-    answer["e, gain ratio, Handedness"] = 0.
+    answer["d, gain ratio, ID"] = 0.232
+    answer["e, gain ratio, Handedness"] = 0.531
 
     # string: one of 'ID' or 'Handedness' based on gain ratio
     # choose the attribute with the largest gain ratio
-    answer["f, which attrib"] = ""
+    answer["f, which attrib"] = "Handedness"
 
     return answer
 
