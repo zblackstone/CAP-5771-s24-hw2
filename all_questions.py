faa91603ed789f94b503b33d5bedb508da361362
zblackstone <144168057+zblackstone@users.noreply.github.com>
2024-02-13 15:54:57 -0500

    Update all_questions.py

--- all_questions.py
+++ all_questions.py
@@ -23,40 +23,40 @@ def question1():
     level2_left = {}
     level2_right = {}
 
-    level1["smoking"] = 1
+    level1["smoking"] = 1.0
     level1["smoking_info_gain"] = 0.278
 
-    level1["cough"] = 0
+    level1["cough"] = 0.0
     level1["cough_info_gain"] = 0.035
 
-    level1["radon"] = 0
+    level1["radon"] = 0.0
     level1["radon_info_gain"] = 0.236
 
-    level1["weight_loss"] = 0
+    level1["weight_loss"] = 0.0
     level1["weight_loss_info_gain"] = 0.029
 
-    level2_left["smoking"] = -1
-    level2_left["smoking_info_gain"] = -1
-    level2_right["smoking"] = -1
-    level2_right["smoking_info_gain"] = -1
+    level2_left["smoking"] = -1.0
+    level2_left["smoking_info_gain"] = -1.0
+    level2_right["smoking"] = -1.0
+    level2_right["smoking_info_gain"] = -1.0
 
-    level2_left["radon"] = 1
+    level2_left["radon"] = 1.0
     level2_left["radon_info_gain"] = 0.722
 
-    level2_left["cough"] = -1
-    level2_left["cough_info_gain"] = -1
+    level2_left["cough"] = -1.0
+    level2_left["cough_info_gain"] = -1.0
 
-    level2_left["weight_loss"] = -1
-    level2_left["weight_loss_info_gain"] = -1
+    level2_left["weight_loss"] = -1.0
+    level2_left["weight_loss_info_gain"] = -1.0
 
-    level2_right["radon"] = -1
-    level2_right["radon_info_gain"] = -1
+    level2_right["radon"] = -1.0
+    level2_right["radon_info_gain"] = -1.0
 
-    level2_right["cough"] = 1
+    level2_right["cough"] = 1.0
     level2_right["cough_info_gain"] = 0.722
 
-    level2_right["weight_loss"] = -1
-    level2_right["weight_loss_info_gain"] = -1
+    level2_right["weight_loss"] = -1.0
+    level2_right["weight_loss_info_gain"] = -1.0
 
     answer["level1"] = level1
     answer["level2_left"] = level2_left
